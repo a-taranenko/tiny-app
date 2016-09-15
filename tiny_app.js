8633c788150fdfbb795712332c657f727938736d
Anton Taranenko <taranenko.anton@gmail.com>
2016-09-15 21:13:21 +0000

    Adds an updated url shortening js file

--- tiny_app.js
+++ tiny_app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -23,6 +26,16 @@ function generateRandomString() {
 
   return randomString;
 }
+
+app.get('/urls', function(req, res) {
+  let templateVars = { urls: urlDatabase }; //whatever I a passing (templateVars) becomes urls in .ejs
+  res.render('urls_index', templateVars);
+});
+
+// app.get("/urls/:id", (req, res) => {
+//   let templateVars = { shortURL: req.params.id };
+//   res.render("urls_show", templateVars);
+// });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -43,17 +56,37 @@ app.post("/urls/create", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let statement = { info: "A short URL has been added to the database!"};
+  let statement = { urlInfo: req.url.substring(6, 12),
+    longUrlInfo: urlDatabase[req.url.substring(6, 12)]};
+
   res.render("urls_show", statement);
 });
 
+app.put("/urls/:id", (req, res) => {
+  let uniqueURL = req.url.substring(6, 12);
+  //console.log(res.req.body['long URL']);
+
+  urlDatabase[uniqueURL] = res.req.body["long URL"];
+
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
-  let key = req.url.substring(3,9);
+  let key = req.url.substring(3, 9);
   let longURL = urlDatabase[key];
 
   // console.log(key);
   // console.log(longURL);
   res.redirect(longURL);
+});
+
+app.delete("/urls/:id", (req, res) => {
+  //Access proper object key and delete that key-value pair
+  let keyToDelete = req.url.substring(6, 12);
+  delete urlDatabase[keyToDelete];
+
+  console.log(urlDatabase);
+  res.redirect("/urls");
 });
 
 app.listen(8080);
